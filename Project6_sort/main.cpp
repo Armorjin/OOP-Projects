b63b1d39857ff5e31444215fe23c9f9bda6d6bec
Armorjin <armorcoon@gmail.com>
2023-12-01 13:25:26 +0300

    init Project6

---
+++ Project6_sort/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <vector>
+
+//cmake -G  "MinGW Makefiles" ../
+
+void sort(){
+
+
+}
+int main(){
+
+    std::cout<< "Tests\n";
+    sort();
+    return 0;
+}
